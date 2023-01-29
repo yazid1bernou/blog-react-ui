1585a9c5cc48a53d58fd5d565d71ed5e33a6a97e
bernou <bernou_y@yahoo.fr>
2023-01-29 22:05:28 +0100

    sidebar , posts and post

--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './posts.css'
+import Post from "../post/Post";
 export default function Posts() {
   return (
     <div className='posts'>
         Posts
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> 
     </div>
-  )
+  );
 }
 
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
       <div className='sidebar'>
             <div className='sidebarItem'>
               <span className='sidebarTitle'>ABOUT ME</span>
-              <img src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              <img src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg" alt="" /> 
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum earum repudiandae deserunt 
                 assumenda non voluptatem.
               </p>
