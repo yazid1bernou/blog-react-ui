e09b6c82e6d050cf43f77ae6521a968ae73353a6
Yazid <yazid1bernou@gmail.com>
2023-02-01 16:54:31 +0100

    create Login and register page and create router also

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 
 import Topbar from "./components/topbar/Topbar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
-/* import Home from "./pages/home/Home"; */
+import Home from "./pages/home/Home"; 
 import Single from "./pages/single/Single"
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 function App() {
+  const user =  true; 
   return (
-    <>
+    <Router>
          <Topbar />
-         <Settings />
+         <Switch>
+            <Route exact path="/"> 
+               <Home />
+            </Route>
+            <Route path="/register"> {user ? <Home /> : <Register /> }  </Route>
+            <Route path="/login" > {user ?  <Home /> :  <Login /> }     </Route>
+            <Route path="/write" > { user ? <Write /> :   <Register /> } </Route>
+            <Route path="/settings" >{ user ? <Settings /> :   <Register /> } </Route>
+            <Route path="/post/:postId" > 
+              <Single />
+            </Route>
+              
+            
+         </Switch>
          
        
-    </>
+    </Router>
   );
 }
 
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './topbar.css';
-
+import { Link } from "react-router-dom";
 export default function Topbar () {
-
+    const user = false ;
     return (
         <div className="top">
                 <div className="topLeft">
@@ -13,7 +13,9 @@ export default function Topbar () {
                 </div>
                 <div className="topCenter">
                     <ul className="topList">
-                        <li className="topListItem">HOME</li>
+                        <li className="topListItem">
+                           <Link to={'/'} className='link'> HOME</Link>  
+                        </li>
                         <li className="topListItem">ABOUT</li>
                         <li className="topListItem">CONTACT</li>
                         <li className="topListItem">WRITE</li>
